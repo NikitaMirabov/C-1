145183749ce9ef08f953601fb50ffccaa864bc1a
NikitaMirabov <nikitamirabov@gmail.com>
2023-02-23 12:53:29 +0300

    Здоровоемся с особенным пользователем

---
+++ Ex5_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Как тебя зовут, солнце?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "никита")
+{
+    Console.WriteLine("Привет, красавчик, у тебя все получится!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write(", а где Никита?(");
+}
