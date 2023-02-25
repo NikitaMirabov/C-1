7d36e6db9ec0a875577a2940fef56439d31513ad
NikitaMirabov <nikitamirabov@gmail.com>
2023-02-25 14:17:20 +0300

    Ex_10_MethodArray

---
+++ Ex_10_MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿int[]array = {11, 21, 31, 34, 35, 37, 23, 42, 38};
+
+int n = array.Length;
+int find = 23;
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    //index= index+1;
+    index++;
+}
+
+
